4f27244349eee25493658a15908125a6c8d0b94b
Nipun Aggarwal <nipunaggarwal201@gmail.com>
2019-07-02 21:03:05 +0530

    First

---
+++ app/src/main/java/com/example/nipun/facebookapp/Splash_Screen.java
@@ -0,0 +1,36 @@
+package com.example.nipun.facebookapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash__screen);
+
+        Thread th = new Thread()
+        {
+            @Override
+            public void run() {
+                try{
+
+                    sleep(3000);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+
+                }
+                finally
+                {
+                    Intent var = new Intent(Splash_Screen.this, Facebook.class);
+                    startActivity(var);
+                }
+            }
+        };
+        th.start();
+    }
+}
